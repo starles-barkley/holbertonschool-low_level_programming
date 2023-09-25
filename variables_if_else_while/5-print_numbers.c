76300ab84b0ad55c23b241763517ffecae886667
root <starlee.jiles@icloud.com>
2023-09-25 16:29:02 -0700

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
+	puchar('\n');
+	return(0);
+}
