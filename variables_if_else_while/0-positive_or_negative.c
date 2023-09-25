57c8236763351c79935363eaa9b8e33382874bd6
root <starlee.jiles@icloud.com>
2023-09-25 14:11:02 -0700

    change variable to n

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,16 +7,16 @@ int main(void)
 	int x;
 
 	srand(time(0));
-	x = rand()  - RAND_MAX / 2;
+	n = rand()  - RAND_MAX / 2;
 	
-	printf("%d \n", x);
+	printf("%d \n", n);
        
 	/* Our own code will go here */
-	if (x > 0)
+	if (n > 0)
 	{
 		printf ("The number is positive \n");
 	}
-	else if (x < 0)
+	else if (n < 0)
 	{
 		printf("The number is negative \n");
 	}
