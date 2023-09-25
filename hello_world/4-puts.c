21e9acc9d276560c29fd511bd7df917d2abc5947
root <starlee.jiles@icloud.com>
2023-09-25 12:07:12 -0700

    space between zero & return; added comments

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/*
+ * main is the entry point
+ * Return = 0 for success
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
