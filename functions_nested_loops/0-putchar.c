45f8eb038f755271f71f223905eaa112b0e73ac8
root <starlee.jiles@icloud.com>
2023-09-25 19:22:25 -0700

    fix betty style

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * main - Print _putchar
+ *
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	printf("_putchar\n");
-	    return (0);
+	return (0);
 }
