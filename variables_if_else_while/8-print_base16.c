df80564c6407e9ccac539a8d024dbc02b5ea06ed
root <starlee.jiles@icloud.com>
2023-09-25 17:16:37 -0700

    fix spaces

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * main - A program that prints all the numbers of base 16.
- * 
+ *
  * Return : Always 0 (Success)
  */
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 	char character;
 	int num;
 
-	for (num = 0; num<= 9; num++)
+	for (num = 0; num <= 9; num++)
 		putchar(num + '0');
 	for (character = 'a'; character <= 'f'; character++)
 		putchar(character);
