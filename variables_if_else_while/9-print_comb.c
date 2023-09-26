bd1a81122813e5a0522b24e18e4b06f5e56d1e00
root <starlee.jiles@icloud.com>
2023-09-25 17:54:45 -0700

    continue

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,15 +11,14 @@ int main(void)
 
 	for (num = 0; num <= 9; num++);
 	{
-		putchar(num + '0');
-		if (num != 9);
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
-
-putchar('\n');
-
-return (0);
+	
+	putchar('\n');
+	return (0);
 }
