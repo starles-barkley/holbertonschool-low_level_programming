94500e77a367df75e81b42a52ee3066688766538
root <starlee.jiles@icloud.com>
2023-10-02 14:50:32 -0700

    create 1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digit 0-9
+ *
+ * @c: the character to check
+ *
+ * Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+	if (c <= 9 && c >= 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
