2c5db9c5d15d945ced4b5d83ab5220b5bbb78e37
root <starlee.jiles@icloud.com>
2023-10-02 16:49:21 -0700

    change condition

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -12,13 +12,13 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size <= 0)
+	if (size <= 0; size >= 0)
 	{
 		_putchar('\n');
 	}
 	for (i = 0; i < size; j++)
 	{
-		for (j = 0; j < (size); j++)
+		for (j = 0; j < size; j++)
 		{
 			_putchar('#');
 		}
