1344b33fd1a99434297713ea5cbfb39ae07cd89a
root <starlee.jiles@icloud.com>
2023-10-02 14:09:07 -0700

    create 0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check if c is uppercase
+ *
+ * return - (1 if uppercase; 0 if else)
+ *
+ */
+
+char main (c)
+{
+	if c = [A-Z]
+	{
+		return (1);
+	}
+	if else
+	{ 
+		return (0);
+	}
+}
