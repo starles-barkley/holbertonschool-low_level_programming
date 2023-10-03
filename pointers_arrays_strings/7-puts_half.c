aa24bb1e3679a632463ec1213bd0b9d4f9abd519
root <starlee.jiles@icloud.com>
2023-10-02 20:52:17 -0700

    fix 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,27 +8,17 @@
 
 void puts_half(char *str)
 {
-	int c, a;
-	char l;
+	int string = 0, n;
 
-	for (c = 0; str[c] != 0; c++)
-	{
-	}
-	if (c % 2 == 0)
-	{
-		for ((a = c / 2); str[a] != 0; a++)
-		{
-			l = str[a];
-			_putchar(l);
-		}
-	}
-	else
-	{
-		for ((a = (c - 1) / 2); str[a] != 0; a++)
-		{
-			l = str[a];
-			_putchar(l);
-		}
-	}
+	while (str[string] != '\0')
+		string++;
+	if (string + 1 % 2 != '0')
+		n = (string - 1) / 2;
+	else 
+		n = (string / 2);
+	n++;
+
+	for (string = n; str[string] != '\0'; string++)
+		_putchar(str[string]);
 	_putchar('\n');
 }
