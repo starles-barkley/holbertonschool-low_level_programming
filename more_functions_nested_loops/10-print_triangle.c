f4f0a1a30a8bc4015b6081d15486f1a50a09f5a2
root <starlee.jiles@icloud.com>
2023-10-02 17:15:15 -0700

    create 10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: integer for parameters of function
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int x, y = 0;
+	int space;
+
+	if (size > 0)
+	{
+		while (y < size)
+		{
+			space = size - y - 1;
+			while (x < size)
+			{
+				if (space > x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+				x++;
+			}
+			y++;
+			x = 0;
+			_putchar ('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
