f996c3ffbe5b3ca2df8bd963ec55a7cddb88fa51
root <starlee.jiles@icloud.com>
2023-10-02 20:24:35 -0700

    create 7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: parameters of the function
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int c, a;
+	char l;
+
+	for (c = 0; str[c] != 0; c++)
+	{
+	}
+	if (c % 2 == 0)
+	{
+		for ((a = c / 2); str[a] != 0; a++)
+		{
+			l = str[a];
+			_putchar(l);
+		}
+	}
+	else
+	{
+		for ((a = (c - 1) / 2); str[a] != 0; a++)
+		{
+			l = str[a];
+			_putchar(l);
+		}
+	}
+	_putchar('\n');
+}
