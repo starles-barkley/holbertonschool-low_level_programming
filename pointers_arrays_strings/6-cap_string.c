7a35360a2d038aab09b52ef9dd52ca989e83ed51
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-10-09 22:05:05 -0500

    Update 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,34 @@
-#include "main.h"
-
-/**
- * cap_string - capitalizes all words in a string
- * @s: string
- * Return: address of s
- */
-
-char *cap_string(char *s)
-{
-	int index = 0;
-
-	while (str[index])
-	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-		if (str[index - 1] == ' ' ||
-			str[index - 1] == '\t' ||
-			str[index - 1] == '\n' ||
-			str[index - 1] == ',' ||
-			str[index - 1] == ';' ||
-			str[index - 1] == '.' ||
-			str[index - 1] == '!' ||
-			str[index - 1] == '?' ||
-			str[index - 1] == '"' ||
-			str[index - 1] == '(' ||
-			str[index - 1] == ')' ||
-			str[index - 1] == '{' ||
-			str[index - 1] == '}' ||
-			index == 0)
-			str[index] -= 32;
-		index++;
-	}
-	return (str);
-}
+#include "main.h"                                           
+                                                            
+/**                                                         
+ * cap_string - capitalizes all words in a string           
+ * @s: string                                               
+ * Return: address of s                                     
+ */                                                         
+                                                            
+char *cap_string(char *s)                                   
+{                                                           
+        int i = 0, j;                                       
+        char a[] = "\t\n,;.!?\"(){}";                       
+                                                            
+        while (*(s + 1))                                    
+        {                                                   
+                if (*(s + i) >= 'a' && *(s + i) <= 'z')     
+                {                                           
+                        if (i == 0)                         
+                                *(s + i) -= 'a' - 'A';      
+                        else                                
+                        {                                   
+                                for (j = 0; j <= 12; j++)   
+                                {                           
+                                        if (a[j] == *(s + i 
+- 1))                                                       
+                                                *(s + i) -= 
+'a' - 'A';                                                  
+                                }                           
+                        }                                   
+                }                                           
+                i++;                                        
+        }                                                   
+        return (s);                                         
+}      
