1872021879d733d9eb41e4f14e96e93024c22d10
root <starlee.jiles@icloud.com>
2023-10-09 20:23:16 -0700

    remove whitespace

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
-		dest[i + j] = src [j];
+		dest[i + j] = src[j];
 	}
 	dest[i + j] = '\0';
 	return (dest);
