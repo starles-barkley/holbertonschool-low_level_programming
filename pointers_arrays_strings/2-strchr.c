0a90299d96d5163bf12dbd0ddda81e7de6760b73
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-10-09 23:55:14 -0500

    Update 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * _strchr - locates character in a string
- * @s: pointer
- * @c: character
- * Return: pointer to first occurrence of c, or null if not found
- */
-
+*_strchr - Returns a pointer to the first occurrence
+*of the character c in the string s, or NULL if the
+*character is not found
+*
+*@s:string targeted
+*@c:character targeted
+*
+*Return: returns pointer to first occcurence of c
+*/
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
-	return (NULL);
+	int i;
+
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+		;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
 }
