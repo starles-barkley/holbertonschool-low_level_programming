33c73d257031cb29d2cec9c86b368653749103a5
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-10-10 22:29:04 -0500

    Update 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,61 +1,36 @@
-#include "holberton.h"
+#include "main.h"
 /**
- *_strlen - reset number
- *Description: This function return a length for some string
- *@s: pointer char
- *Return: int length
+ *cap_string - capitalizes every first letter of a word in a string.
+ *separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *@s: pointer to string.
+ *
+ *Return: pointer to s.
  */
-
-int _strlen(char *s)
+char *cap_string(char *s)
 {
-	int len = 0;
+	int count;
 
-	while (*s++)
-	{
-		len++;
-	}
-	return (len);
-}
-/**
- *cap_string - changes all lowercase letters
- *@s1: pointer parameter"
- *Description: changes all lowercase letters
- *Return: return pointer
- */
-char *cap_string(char *s1)
-{
-	int i, j;
-
-	for (i = 0; i < _strlen(s1) - 1; i++)
-	{
-		if (
-			s1[i] == ' ' ||
-			s1[i] == '\t' ||
-			s1[i] == '\n' ||
-			s1[i] == ',' ||
-			s1[i] == ';' ||
-			s1[i] == '.' ||
-			s1[i] == '!' ||
-			s1[i] == '?' ||
-			s1[i] == '"' ||
-			s1[i] == '(' ||
-			s1[i] == ')' ||
-			s1[i] == '{' ||
-			s1[i] == '}' ||
-			i == 0
-		)
+/*  scan through string */
+	count = 0;
+	while (s[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+		if (s[0] >= 97 && s[0] <= 122)
 		{
-			for (j = 'a'; j <= 'z'; j++)
+			s[0] = s[0] - 32;
+		}
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
 			{
-				if (s1[i + 1] == j && i != 0)
-				{
-					s1[i + 1] = j - 32;
-				} else if (s1[i] == j && i == 0)
-				{
-					s1[i] = j - 32;
-				}
+				s[count + 1] = s[count + 1] - 32;
 			}
 		}
+		count++;
 	}
-	return (s1);
+	return (s);
 }
