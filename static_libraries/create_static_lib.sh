cdfd5e1b0375e9ec277db10e6b8cec5ae2dd53f1
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-10-19 21:05:39 -0500

    Update create_static_lib.sh

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,5 +1,5 @@
-   #!/bin/bash
-   # This script compiles all .c files in the current directory into .o files
-   gcc -c *.c
-   # This script creates a static library from all .o files
-   ar -rc liball.a *.o
+#!/bin/bash
+gcc -c *.c
+ar rc liball.a *.o
+ranlib liball.a
+rm -f *.o
