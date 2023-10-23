f8be62fdd97b669266686bcac8faf4500c6a4808
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-10-22 21:47:20 -0500

    Create 0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c) {
+  
+    if (size == 0) {
+        return NULL;
+    }
+  
+    char *array = (char*) malloc(size * sizeof(char));
+
+    if (array == NULL) {
+        return NULL;
+    }
+  
+    for (unsigned int i = 0; i < size; i++) {
+        array[i] = c;
+    }
+
+    return array;
+}
