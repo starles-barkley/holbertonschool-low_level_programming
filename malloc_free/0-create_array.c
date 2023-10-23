2c3fd2fb371129942075b0277e97bf0813fe03e3
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-10-22 21:52:47 -0500

    Update 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,19 +1,18 @@
 #include <stdlib.h>
 
 char *create_array(unsigned int size, char c) {
-  
     if (size == 0) {
-        return NULL;
+        return NULL; // Return NULL if size is 0
     }
-  
-    char *array = (char*) malloc(size * sizeof(char));
+
+    char *array = (char *)malloc(size * sizeof(char)); // Allocate memory for the array
 
     if (array == NULL) {
-        return NULL;
+        return NULL; // Return NULL if memory allocation fails
     }
-  
+
     for (unsigned int i = 0; i < size; i++) {
-        array[i] = c;
+        array[i] = c; // Initialize the array with the specified character
     }
 
     return array;
