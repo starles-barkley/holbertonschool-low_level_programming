4bfe8cf9566223d177d17f41b22da5c9ed13646e
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-11-14 02:08:38 +0000

    fix 2-add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -26,4 +26,24 @@ list_t *add_node(list_t **head, const char *str)
 	char *content;
 	list_t *new;
 
-
+	if (str == NULL || head == NULL)
+		return (NULL);
+	len = _strlen(str);
+	new = *head;
+	content = malloc((len + 1) * sizeof(char));
+	if (content == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		content[i] = str[i];
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(content);
+		return (NULL);
+	}
+	new->str = content;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
