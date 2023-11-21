ae71d7c3f7a84346cf750d76c59a2b848c5b677f
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-11-21 02:39:26 +0000

    fix prototypes in header

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,22 +1,25 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Description: doubly linked list node structure
+ *
  */
-typedef struct list_s
+typedef struct dlistint_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
@@ -29,4 +32,3 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif
-
